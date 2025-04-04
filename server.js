6df4bf9286e318786a412994ec8ff8061f5d6c86
arthursilva1004 <jogossrthurotavio@gmail.com>
2025-04-04 17:03:42 -0300

    apaguei algumas coisas do server desnecessarias

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 require('dotenv').config();
 const express = require('express');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
 const cors = require('cors');
 
 const app = express();
